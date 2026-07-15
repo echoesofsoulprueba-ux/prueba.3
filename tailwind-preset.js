5eff34c3ce3f76577ae1e779ac7137826b8eb1b0
echoesofsoulprueba-ux <echoesofsoulprueba@gmail.com>
2026-07-15 11:44:02 -0300

    update: tailwind-preset.js

---
+++ tailwind-preset.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      "colors": {
+        "card": "#FFFFFF",
+        "error": "#EF4444",
+        "muted": "#F1F5F9",
+        "border": "#E2E8F0",
+        "sidebar": "#1E293B",
+        "success": "#22C55E",
+        "warning": "#F97316",
+        "secondary": "#FF07AF",
+        "background": "#FFFFFF",
+        "foreground": "#1E293B",
+        "sidebar-active": "#FFE600",
+        "card-foreground": "#1E293B",
+        "error-foreground": "#000000",
+        "muted-foreground": "#334155",
+        "primary-foreground": "#000000",
+        "sidebar-foreground": "#F1F5F9",
+        "success-foreground": "#000000",
+        "warning-foreground": "#000000",
+        "secondary-foreground": "#000000",
+        "sidebar-active-foreground": "#000000",
+        "accent": {
+          "50": "#fbf4f9",
+          "100": "#f9e7f3",
+          "200": "#f7c5e7",
+          "300": "#f797d8",
+          "400": "#fb56c6",
+          "500": "#ff1ab5",
+          "600": "#eb009f",
+          "700": "#bd0582",
+          "800": "#910865",
+          "900": "#5e0842"
+        },
+        "neutral": {
+          "50": "#f7f7f8",
+          "100": "#eff0f0",
+          "200": "#dcdde0",
+          "300": "#c3c6cb",
+          "400": "#a2a7af",
+          "500": "#838a96",
+          "600": "#6c737f",
+          "700": "#595f68",
+          "800": "#474b52",
+          "900": "#2f3237"
+        },
+        "primary": {
+          "50": "#fbfaf4",
+          "100": "#f9f7e7",
+          "200": "#f7f2c5",
+          "300": "#f7ed97",
+          "400": "#fbeb56",
+          "500": "#ffe91a",
+          "600": "#ebd400",
+          "700": "#bdab05",
+          "800": "#918408",
+          "900": "#5e5608",
+          "DEFAULT": "#FFE600"
+        }
+      },
+      "fontFamily": {
+        "sans": [
+          "Lato",
+          "sans-serif"
+        ]
+      }
+    },
+  },
+}
